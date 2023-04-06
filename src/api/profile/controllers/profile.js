e40974bdc82d57f6887c8427cf6a0aee5bd3214a
Vova Rodin <mynameisvova@yahoo.com>
2023-04-06 16:00:41 +0300

    overlap profile controller @find

--- src/api/profile/controllers/profile.js
+++ src/api/profile/controllers/profile.js
@@ -6,4 +6,31 @@
 
 const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::profile.profile')
+const uid = 'api::profile.profile'
+module.exports = createCoreController(uid, {
+  async find(ctx) {
+    if (!ctx.state.user) return // if is unauthorized
+
+    const entities = await strapi.entityService.findMany(uid, {
+      filters: { user: ctx.state.user.id },
+    })
+
+    // create profile if entity not found
+    if (!entities.length) {
+      const data = {
+        displayName: ctx.state.user.username,
+        user: ctx.state.user,
+      }
+
+      const entity = await strapi.entityService.create(uid, { data })
+      const output = await this.sanitizeOutput(entity, ctx)
+
+      return this.transformResponse(output, null)
+    }
+
+    // return founded entity
+    const output = await this.sanitizeOutput(entities[0], ctx)
+
+    return this.transformResponse(output, null)
+  },
+})
