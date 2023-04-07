6f9f46e862e0e8361819c82f2c9cb250ecdaf13e
Vova Rodin <mynameisvova@yahoo.com>
2023-04-07 15:49:57 +0300

    post api error handling

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -5,6 +5,7 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories
+const { ForbiddenError, NotFoundError } = require('@strapi/utils/lib/errors')
 const { slugify } = require('#src/utilities/slugify.js')
 
 const uid = 'api::post.post'
@@ -31,36 +32,42 @@ module.exports = createCoreController(uid, {
       populate: ['user'],
     })
 
-    // if current user is post owner
-    if (found.user.id === ctx.state.user.id) {
-      const input = await this.sanitizeInput(body.data, ctx)
-
-      // generate slug from title
-      if (!input.slug && !found.slug) {
-        input.slug = slugify(input.title || found.title)
-      }
-
-      const entity = await strapi.entityService.update(uid, params.id, {
-        data: input,
-      })
-      const output = await this.sanitizeOutput(entity, ctx)
-
-      return this.transformResponse(output, null)
+    if (!found) throw new NotFoundError()
+    if (found.user?.id !== ctx.state.user?.id) {
+      throw new ForbiddenError()
     }
+
+    // if current user is post owner
+    const input = await this.sanitizeInput(body.data, ctx)
+
+    // generate slug from title
+    if (!input.slug && !found.slug) {
+      input.slug = slugify(input.title || found.title)
+    }
+
+    const entity = await strapi.entityService.update(uid, params.id, {
+      data: input,
+    })
+    const output = await this.sanitizeOutput(entity, ctx)
+
+    return this.transformResponse(output, null)
   },
 
   async delete(ctx) {
     const { id } = ctx.request.params
-    const { user } = await strapi.entityService.findOne(uid, id, {
+    const found = await strapi.entityService.findOne(uid, id, {
       populate: ['user'],
     })
 
-    // if current user is post owner
-    if (user.id === ctx.state.user.id) {
-      const entity = await strapi.entityService.delete(uid, id, null)
-      const output = await this.sanitizeOutput(entity, ctx)
-
-      return this.transformResponse(output, null)
+    if (!found) throw new NotFoundError()
+    if (found.user?.id !== ctx.state.user?.id) {
+      throw new ForbiddenError()
     }
+
+    // if current user is post owner
+    const entity = await strapi.entityService.delete(uid, id, null)
+    const output = await this.sanitizeOutput(entity, ctx)
+
+    return this.transformResponse(output, null)
   },
 })
